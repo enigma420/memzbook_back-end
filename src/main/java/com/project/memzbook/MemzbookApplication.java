c5f2f3f9127ba58def993d38e20520e40223c2c0
Dominik <enigma3420@gmail.com>
2021-09-13 20:56:42 +0200

    Initial commit

---
+++ src/main/java/com/project/memzbook/MemzbookApplication.java
@@ -0,0 +1,13 @@
+package com.project.memzbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemzbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemzbookApplication.class, args);
+    }
+
+}
