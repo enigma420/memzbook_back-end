a4138b91ec35353f020ae0cff85ab2464b3a4aa5
Dominik <enigma3420@gmail.com>
2021-09-14 17:05:09 +0200

    Create User Entity

--- src/main/java/com/project/memzbook/domain/User.java
+++ src/main/java/com/project/memzbook/domain/User.java
@@ -1,4 +1,34 @@
 package com.project.memzbook.domain;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.Value;
+
+import java.time.OffsetDateTime;
+
+/**
+ *  Database model of Users table
+ * */
+@Builder
+@Data
+@Value
 public class User {
+    /* Identifier */
+    Long id;
+    /* User login */
+    String login;
+    /* User password */
+    String password;
+    /* User email */
+    String email;
+    /* Role Identifier (FK with roles table) */
+    int roleId;
+    /* User account status (active/unactive) */
+    boolean active;
+    /* User points */
+    int points;
+    /* User rate */
+    Double rate;
+    /* Creation date */
+    OffsetDateTime createdAt;
 }
