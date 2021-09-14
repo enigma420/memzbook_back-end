4cb8d81439810ae1a1de80b09ba25ad3b448bb16
Dominik <enigma3420@gmail.com>
2021-09-14 18:04:08 +0200

    Create Queries for: 1. Create table if not exists 2. Inserts with example data

---
+++ src/main/resources/db/user/insert/user_insert.sql
@@ -0,0 +1,17 @@
+INSERT INTO users (
+user_id,
+login,
+password,
+email,
+role_id,
+active,
+points,
+rate,
+created_at
+)
+VALUES
+(1,'Hrabia','qwerty123','enigma3420@gmail.com',1,TRUE,0,0.0,'2021-09-14T12:30:05.000'),
+(2,'Elektro','qwerty1234','enig4324@gmail.com',2,TRUE,0,0.0,'2021-09-14T13:30:05.000'),
+(3,'Magneto','qwerty12345','fsfefe@wp.pl',2,TRUE,0,0.0,'2021-09-14T14:30:05.000'),
+(4,'Stefan124','qwerty123456','gdhkr@op.pl',2,TRUE,0,0.0,'2021-09-14T15:30:05.000'),
+(5,'Zosia3420','qwerty1234567','sfshld12@wp.pl',2,TRUE,0,0.0,'2021-09-14T16:30:05.000');
---
+++ src/main/resources/db/user/role/insert/role_insert.sql
@@ -0,0 +1,3 @@
+/* INSERT INTO TABLE ROLES */
+
+INSERT INTO roles (role_id,name) VALUES (1,'Admin'),(2,'User');
